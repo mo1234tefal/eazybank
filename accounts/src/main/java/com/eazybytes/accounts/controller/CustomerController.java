98a97474e567af0b03c86ab4bd252883f01b9e76
mo1234tefal <tmo090461@gmail.com>
2026-04-21 17:47:05 +0200

    initial commit

--- accounts/src/main/java/com/eazybytes/accounts/controller/CustomerController.java
+++ accounts/src/main/java/com/eazybytes/accounts/controller/CustomerController.java
@@ -59,9 +59,12 @@ public class CustomerController {
                                                                         @RequestParam
                                                                        @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
                                                                    String mobileNumber){
-        logger.debug("eazyBank-correlation-id found: {} ", correlationId);
+//        logger.debug("eazyBank-correlation-id found: {} ", correlationId);
+        logger.debug("fetchCustomerDetails method start");
+        CustomerDetailsDto customerDetailsDto = iCustomerService.fetchCustomerDetails(mobileNumber , correlationId);
+        logger.debug("fetchCustomerDetails method end");
         return ResponseEntity.status(HttpStatus.OK)
-                .body(iCustomerService.fetchCustomerDetails(mobileNumber , correlationId));
+                .body(customerDetailsDto);
 
 
 
